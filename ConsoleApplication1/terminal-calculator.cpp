a72e6bd680e1c24df74ef7db70b189c70bb21c1a
F1ndr <159054703+Matveiprokopew@users.noreply.github.com>
2025-11-20 21:08:12 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/terminal-calculator.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream> //inputoutputstream library
+#include <string>
+int main()
+{
+    int sum;
+    std::cout << "Hello World!\n"; //cout-символьный вывод
+
+    std::cout << "Input first number: ";
+    int n1;
+    std::cin >> n1;
+
+    std::cout << "Input second number: ";
+    int n2;
+    std::cin >> n2;
+
+    std::cout << "Input +,-,/,* ";
+    char a;
+    std::cin >> a;
+
+    if (a == '+')
+    {
+        sum = n1 + n2;
+    }
+
+    if (a == '-')
+    {
+        sum = n1 - n2;
+    }
+
+    if (a == '*')
+    {
+        sum = n1 * n2;
+    }
+
+
+    if (a == '/')
+    {
+        sum = n1 / n2;
+    }
+
+    std::cout << sum;
+
+
+    return 0;
+}
+
